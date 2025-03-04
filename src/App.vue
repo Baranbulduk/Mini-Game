63fc4390e7d80f873e41655e0da9826313f15891
Baranbulduk <baranbulduk99@gmail.com>
2025-03-04 10:39:20 +0100

    structure done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <h2>Mini Game</h2>
+    <button class="instructions-button">
+      Show Instructions
+    </button>
+    <div class="game">
+      <div
+        class="player">
+        <div class="arrow-box">
+          <img src="./assets/arrow.png" alt="Arrow" class="arrow" />
+        </div>
+        <h2 class="player-title">
+        </h2>
+        <div class="scores">
+          <p class="score-title">SCORE <span class="score total-score">{{ score }}</span></p>
+          <p class="score-title">CURRENT <span class="score"></span></p>
+        </div>
+      </div>
+    </div>
+    <div class="controls">
+      <div class="controls-buttons">
+        <button class="button">
+          <img src="./assets/play.png" alt="play" class="play-icon" />
+          PLAY
+        </button>
+        <button class="button">
+          <img src="./assets/pause.png" alt="pause" class="pause-icon" />
+          PAUSE
+        </button>
+      </div>
+      <button class="button">
+        <img src="./assets/reset.png" alt="reset" class="reset-icon" />
+        RESET
+      </button>
+      <button class="button">
+        <img src="./assets/addPlayer.png" alt="add player" class="add-player-icon" />
+        ADD PLAYER
+      </button>
+    </div>
+    <div class="modal">
+      <div class="modal-content">
+        <img src="./assets/trophy.png" alt="trophy" class="trophy" />
+        <p class="modal-text">Player {{ winner }} wins!</p>
+        <button class="button close">CLOSE</button>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
